ec156e5614289beef45bb6253e4fb002bed4b172
imStav <saprocontact@gmail.com>
2022-07-22 14:01:59 +0200

    Organizing nav styles

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,16 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header class="fixed bottom-0 py-4 backdrop-blur-xl saturate-150">
-    <nav class="flex justify-center w-screen">
+  <header class="fixed bottom-0">
+    <nav class="navbar">
       <RouterLink
         to="/"
-        class="text-xl font-display mx-4 hover:opacity-60 duration-700"
+        class="nav-link"
         >Ab<em class="font-glitch not-italic">stra</em>ct</RouterLink
       >
       <RouterLink
         to="/about"
-        class="text-xl font-display mx-4 hover:opacity-60 duration-700"
+        class="nav-link"
         >About</RouterLink
       >
     </nav>
