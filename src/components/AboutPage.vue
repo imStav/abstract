ac5dd652f6f874ed15e4ad9a20d31e03479a7938
imStav <saprocontact@gmail.com>
2022-07-30 21:03:27 +0200

    Added a button in the About page
    To let users go back to the gallery directly from this page.

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,19 +1,25 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
 <template>
   <div class="about-layout">
-    <div>
-      <h2 class="heading">
-        About Abstract
-      </h2>
+    <h2 class="heading">
+      About Abstract
+    </h2>
 
-      <p class="text-lg my-8">
-        Abstract is a small web app that provides free wallpapers for personal
-        or commercial use. They are all in a 16 / 9 aspect ratio, and a FHD
-        definition of 1920 x 1080 pixels.
-      </p>
+    <p class="text-lg my-8">
+      Abstract is a small web app that provides free wallpapers for personal
+      or commercial use. They are all in a 16 / 9 aspect ratio, and a FHD
+      definition of 1920 x 1080 pixels.
+    </p>
 
-      <p class="text-lg">
-        The website is subject to changes and will nearly receive UI updates.
-      </p>
-    </div>
+    <p class="text-lg">
+      The website is subject to changes and will nearly receive UI updates.
+    </p>
+
+    <RouterLink to="/gallery" class="back-to-gallery">
+      Back to gallery
+    </RouterLink>
   </div>
 </template>
